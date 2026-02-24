b5fa58716b583214302dbdc1b4cf5f64270f854a
numukobwadiane <numudiane@gmail.com>
2026-02-24 12:03:47 +0200

    Add GraduateStudent class with per-credit tuition and research fee

---
+++ src/Model/GraduateStudent.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class GraduateStudent extends Student {
+
+    private double researchFee;
+
+
+    public GraduateStudent(String name, String email, String studentID, String department, double researchFee) {
+        super(name, email, studentID, department);
+        this.researchFee = researchFee;
+    }
+
+
+    public double getResearchFee() {
+        return researchFee;
+    }
+
+    public void setResearchFee(double researchFee) {
+        this.researchFee = researchFee;
+    }
+
+
+    public double calculateTuition(int totalCredits) {
+        return totalCredits * 300 + researchFee; // 300 per credit + research fee
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Type: Graduate";
+    }
+}
