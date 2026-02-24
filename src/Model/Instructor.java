222fe4d56f97839d72bcd7a632fa1ee03892ff3f
numukobwadiane <numudiane@gmail.com>
2026-02-24 10:09:31 +0200

    I created the Instructor entity, extended Person, and implemented role-specific behavior.

---
+++ src/Model/Instructor.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class Instructor extends Person {
+
+    private String department;
+
+    public Instructor(String name, String email, String department) {
+        super(name, email);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    @Override
+    public String getRole() {
+
+        return "Instructor";
+    }
+}
