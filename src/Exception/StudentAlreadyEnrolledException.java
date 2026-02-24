9af91f298cbedb4f54bde7148db6eac371bb56ab
numukobwadiane <numudiane@gmail.com>
2026-02-24 16:28:02 +0200

    Add custom exceptions for enrollment validation

---
+++ src/Exception/StudentAlreadyEnrolledException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class StudentAlreadyEnrolledException extends Exception {
+    public StudentAlreadyEnrolledException(String message) {
+        super(message);
+    }
+}
